520db880f36a1e1c3ad6582894ea1675eb747bf9
joanlamrack <joanlamrack@gmail.com>
2018-07-17 00:26:45 +0700

    added code implementations

--- tree_grove.js
+++ tree_grove.js
@@ -1,32 +1,104 @@
-const MangoTree = require('./mango_tree.js');
-const AppleTree = require('./apple_tree.js');
-const PearTree = require('./pear_tree.js');
+const imports = require('./mango_tree.js');
+const MangoTree = imports.MangoTree;
+const AppleTree = imports.AppleTree;
+const PearTree = imports.PearTree;
 
-class TreeGrove {}
+class TreeGrove {
+	constructor(){
+		this._grove = [];
+		console.log("input your trees data !\nparameter ke-1: nama pohon\nparameter ke-2: umur pohon ketika ditanam di taman tersebut\npamareter ke-3: tinggi pohon pertama kali ketika ditanam di taman tersebut\nparameter ke-4: umur mature pohon tersebut\n");
+	}
 
-var grove = new TreeGrove()
+	inputTree(treeKind, age, height, matureAge){
+		if(treeKind === "MangoTree"){
+			this._grove.push(new MangoTree(age,height,matureAge));
+		}
+		else if(treeKind === "PearTree"){
+			this._grove.push(new PearTree(age,height,matureAge));
+		}
+		else if(treeKind === "AppleTree"){
+			this._grove.push(new AppleTree(age,height,matureAge));
+		}
+		
+	}
+
+	get grove(){
+		return this._grove;
+	}
+
+	showAges(){
+		for(let tree of this.grove){
+			console.log(`${tree.treeKind} Tree : ${tree.age} years old`);
+		}
+	}
+
+	showTrees(){
+		let treeNames = [];
+		for(let tree of this.grove){
+			treeNames.push(tree.treeKind);
+		}
+		return treeNames;
+	}
+
+	showMatureTrees(){
+		let treeNames = [];
+		for(let tree of this.grove){
+			if(tree.age >= tree.matureAge && tree.healthStatus) treeNames.push(tree.treeKind);
+		}
+		return treeNames;
+	}
+
+	showDeadTrees(){
+		let treeNames = [];
+		for(let tree of this.grove){
+			if(!tree.healthStatus) treeNames.push(tree.treeKind);
+		}
+		return treeNames;
+	}
+
+	nextYear(){
+		for(let tree of this.grove){
+			tree.grow();
+			tree.produceFruits();
+		}
+	}
+
+}
+
+var grove = new TreeGrove();
 // input your trees data !
 // parameter ke-1: nama pohon
 // parameter ke-2: umur pohon ketika ditanam di taman tersebut
 // pamareter ke-3: tinggi pohon pertama kali ketika ditanam di taman tersebut
 // parameter ke-4: umur mature pohon tersebut
 // parameter ke-5: healthStatus dari pohon tersebut ketika ditanam
-grove.inputTree("MangoTree", 3, 1.8, 7, true)
-grove.inputTree("MangoTree", 5, 2.4, 12 ,true)
-grove.inputTree("AppleTree", 4, 1.2, 5, true)
-grove.inputTree("PearTree", 7, 2, 15, true)
+grove.inputTree("MangoTree", 3, 1.8, 7);
+grove.inputTree("MangoTree", 5, 2.4, 12);
+grove.inputTree("AppleTree", 4, 1.2, 5);
+grove.inputTree("PearTree", 7, 2, 15);
 
 // next year
-grove.nextYear()
-
+grove.nextYear();
+grove.nextYear();
+grove.nextYear();
+grove.nextYear();
+grove.nextYear();
+grove.nextYear();
+grove.nextYear();
+grove.nextYear();
+grove.nextYear();
+grove.nextYear();
+grove.nextYear();
+grove.nextYear();
 // show trees ages
-grove.showAges()
+grove.showAges();
 
 // show trees
-grove.showTrees()
+console.log(grove.showTrees());
 
 // show trees
-grove.showMatureTrees()
+console.log(grove.showMatureTrees());
 
 // show trees
-grove.showDeadTrees()
+console.log(grove.showDeadTrees());
+
